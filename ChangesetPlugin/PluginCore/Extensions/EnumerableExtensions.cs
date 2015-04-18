915ed39668210db7417a9131edc7158dbde99329
Peter <peterrexj@hotmail.com>
2015-04-18 03:15:56 +0000

    plugin core

---
+++ ChangesetPlugin/PluginCore/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,29 @@
+﻿using PluginCore.Extensions;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace PluginCore.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        [System.Diagnostics.DebuggerStepThrough]
+        public static IEnumerable<TItem> DistinctBy<TItem, TDistinctValue>(this IEnumerable<TItem> items, Func<TItem, TDistinctValue> map, IEqualityComparer<TDistinctValue> distinctValueComparer = null)
+        {
+            if (distinctValueComparer == null)
+                distinctValueComparer = EqualityComparer<TDistinctValue>.Default;
+
+            var comparer = EqualityComparerEx.Create<TItem>((a, b) => distinctValueComparer.Equals(map.Invoke(a), map.Invoke(b)), a => distinctValueComparer.GetHashCode(map.Invoke(a)));
+            return items.EmptyIfNull().Distinct(comparer);
+        }
+
+        [System.Diagnostics.DebuggerStepThrough]
+        public static IEnumerable<T> EmptyIfNull<T>(this IEnumerable<T> items)
+        {
+            return items ?? Enumerable.Empty<T>();
+        }
+    }
+}
---
+++ ChangesetPlugin/PluginCore/Extensions/EqualityComparerEx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PluginCore.Extensions
+{
+    public static class EqualityComparerEx
+    {
+        public static IEqualityComparer<T> Create<T>(Func<T, T, bool> equals, Func<T, int> getHashCode)
+        {
+            return new JEqualityComparer<T>(equals, getHashCode);
+        }
+    }
+
+    public class JEqualityComparer<T> : IEqualityComparer<T>
+    {
+        Func<T, T, bool> _equals;
+        Func<T, int> _getHashCode;
+        public JEqualityComparer(Func<T, T, bool> equals, Func<T, int> getHashCode)
+        {
+            _equals = equals;
+            _getHashCode = getHashCode;
+        }
+
+        #region IEqualityComparer<T> Members
+
+        bool IEqualityComparer<T>.Equals(T x, T y)
+        {
+            return _equals(x, y);
+        }
+
+        int IEqualityComparer<T>.GetHashCode(T obj)
+        {
+            return _getHashCode(obj);
+        }
+
+        #endregion
+    }
+}
