b7cf713c45bd8367b6c080be5c25ab1f18006b92
Peter <peterrexj@hotmail.com>
2015-06-08 10:33:27 +0000

    move to controller and model

---
+++ ChangesetPlugin/ChangesetViewer.UI.Test/Infra/ChangesetModel.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.TeamFoundation.Server;
+using Microsoft.TeamFoundation.VersionControl.Client;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangesetViewer.UI.Test.Infra
+{
+    public class ChangesetModel
+    {
+        //public IEnumerable<Identity> AllUsersInTfs { get; set; }
+        public ObservableCollection<Identity> UserCollectionInTFS { get; set; }
+        public ObservableCollection<Changeset> ChangeSetCollection { get; set; }
+
+        public ChangesetModel()
+        {
+            UserCollectionInTFS = new ObservableCollection<Identity>();
+            ChangeSetCollection = new ObservableCollection<Changeset>();
+        }
+
+
+        public int ChangeSetCollectionCount()
+        {
+            return ChangeSetCollection.Count;
+        }
+
+    }
+}
