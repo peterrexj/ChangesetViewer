fe8ce9b845a1c27ffa110c2c555d9d24f848d8b4
Peter <peterrexj@hotmail.com>
2015-06-26 07:40:13 +0000

    regular check ins

---
+++ ChangesetViewer.Core/UI/Consts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangesetViewer.Core.UI
+{
+    public static class Consts
+    {
+        public enum SearchCommentType
+        {
+            Exact,
+            Keyword
+        }
+    }
+}
