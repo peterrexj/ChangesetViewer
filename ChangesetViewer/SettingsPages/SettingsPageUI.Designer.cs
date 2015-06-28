0fa9282c4872a4bd3439e058a4a85833ab52c721
Peter <peterrexj@hotmail.com>
2015-06-28 09:40:43 +0000

    temp

--- ChangesetViewer/SettingsPages/SettingsPageUI.Designer.cs
+++ ChangesetViewer/SettingsPages/SettingsPageUI.Designer.cs
@@ -29,18 +29,18 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.chkUseVStfsInfo = new System.Windows.Forms.CheckBox();
+            this.txtDefaultSearchPath = new System.Windows.Forms.TextBox();
             this.grpServerCredentials = new System.Windows.Forms.GroupBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.chkUseVStfsInfo = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.txtDefaultSearchPath = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
@@ -60,24 +60,14 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Settings";
             // 
-            // label1
+            // txtDefaultSearchPath
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 169);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(100, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Server Search Path";
-            // 
-            // chkUseVStfsInfo
-            // 
-            this.chkUseVStfsInfo.AutoSize = true;
-            this.chkUseVStfsInfo.Location = new System.Drawing.Point(14, 17);
-            this.chkUseVStfsInfo.Name = "chkUseVStfsInfo";
-            this.chkUseVStfsInfo.Size = new System.Drawing.Size(240, 17);
-            this.chkUseVStfsInfo.TabIndex = 1;
-            this.chkUseVStfsInfo.Text = "Use Visual Studio connected TFS information";
-            this.chkUseVStfsInfo.UseVisualStyleBackColor = true;
+            this.txtDefaultSearchPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDefaultSearchPath.Location = new System.Drawing.Point(116, 166);
+            this.txtDefaultSearchPath.Name = "txtDefaultSearchPath";
+            this.txtDefaultSearchPath.Size = new System.Drawing.Size(193, 20);
+            this.txtDefaultSearchPath.TabIndex = 3;
             // 
             // grpServerCredentials
             // 
@@ -94,41 +84,14 @@
             this.grpServerCredentials.TabStop = false;
             this.grpServerCredentials.Text = "Server Settings";
             // 
-            // textBox1
+            // label4
             // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(81, 19);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(214, 20);
-            this.textBox1.TabIndex = 0;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox2.Location = new System.Drawing.Point(81, 45);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(214, 20);
-            this.textBox2.TabIndex = 1;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox3.Location = new System.Drawing.Point(81, 71);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(214, 20);
-            this.textBox3.TabIndex = 2;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(15, 22);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(63, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Server URL";
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 74);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Password";
             // 
             // label3
             // 
@@ -139,14 +102,60 @@
             this.label3.TabIndex = 4;
             this.label3.Text = "User Name";
             // 
-            // label4
+            // label2
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(15, 74);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(53, 13);
-            this.label4.TabIndex = 5;
-            this.label4.Text = "Password";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Server URL";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox3.Location = new System.Drawing.Point(81, 71);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(214, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(81, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(214, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(81, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(214, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // chkUseVStfsInfo
+            // 
+            this.chkUseVStfsInfo.AutoSize = true;
+            this.chkUseVStfsInfo.Location = new System.Drawing.Point(14, 17);
+            this.chkUseVStfsInfo.Name = "chkUseVStfsInfo";
+            this.chkUseVStfsInfo.Size = new System.Drawing.Size(240, 17);
+            this.chkUseVStfsInfo.TabIndex = 1;
+            this.chkUseVStfsInfo.Text = "Use Visual Studio connected TFS information";
+            this.chkUseVStfsInfo.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 169);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Default Server path";
             // 
             // label5
             // 
@@ -166,15 +175,6 @@
             this.checkBox2.TabIndex = 2;
             this.checkBox2.Text = "Find JIRA tickets in comment";
             this.checkBox2.UseVisualStyleBackColor = true;
-            // 
-            // txtDefaultSearchPath
-            // 
-            this.txtDefaultSearchPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtDefaultSearchPath.Location = new System.Drawing.Point(116, 166);
-            this.txtDefaultSearchPath.Name = "txtDefaultSearchPath";
-            this.txtDefaultSearchPath.Size = new System.Drawing.Size(193, 20);
-            this.txtDefaultSearchPath.TabIndex = 3;
             // 
             // textBox4
             // 
